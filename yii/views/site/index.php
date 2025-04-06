571ac60d611c21b5622871595ba4e6059bd6d9e5
esg <efhsg@live.nl>
2025-04-06 20:01:07 +0200

    Update logo

--- yii/views/site/index.php
+++ yii/views/site/index.php
@@ -10,8 +10,7 @@ $this->title = 'Welcome to Promptmanager';
 
 <div class="site-index">
     <div class="jumbotron text-center bg-light py-5">
-        <img src="<?= Yii::getAlias('@web/images/prompt-manager-logo.png') ?>" alt="Promptmanager Logo" width="200"
-            class="mb-4">
+        <img src="<?= Yii::getAlias('@web/images/prompt-manager-logo.png') ?>" alt="Promptmanager Logo" width="200">
         <h1 class="display-4">Promptmanager</h1>
         <p class="lead">Automate your AI prompts</p>
         <p><?= Html::a('Get Started', ['/project/index'], ['class' => 'btn btn-primary btn-lg']) ?></p>
