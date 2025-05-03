5a10e7a63300797fedc58aa62a383165d538def7
erwin <efhsg@live.nl>
2025-05-03 02:28:34 +0200

    Update Quill integration to use versioned directory structure
    Revised the build scripts and asset paths to include versioning (1.3.7) for Quill files. Added `defer` option for JavaScript loading in `QuillAsset` to optimize loading performance.

--- yii/assets/QuillAsset.php
+++ yii/assets/QuillAsset.php
@@ -5,10 +5,13 @@ use yii\web\AssetBundle;
 
 class QuillAsset extends AssetBundle
 {
-    public $basePath = '@webroot/quill';
-    public $baseUrl  = '@web/quill';
+    public $basePath = '@webroot/quill/1.3.7';
+    public $baseUrl = '@web/quill/1.3.7';
 
     public $css = ['quill.snow.css'];
-    public $js  = ['quill.min.js', 'editor-init.min.js'];
+    public $js = ['quill.min.js', 'editor-init.min.js'];
+
+    public $jsOptions = ['defer' => true];
+
     public $depends = ['yii\web\YiiAsset'];
 }
