5422059c06edb00fdecb7d57840cb5626de99228
erwin <efhsg@live.nl>
2025-05-05 17:56:22 +0200

    Add new build script and update Quill asset dependencies
    Introduce a "build-all" script to streamline the build process, including setup and cleaning steps. Extend QuillAsset dependencies to include highlight.js-related assets, enhancing syntax highlighting capabilities.

--- yii/assets/QuillAsset.php
+++ yii/assets/QuillAsset.php
@@ -8,8 +8,16 @@ class QuillAsset extends AssetBundle
     public $basePath = '@webroot/quill/1.3.7';
     public $baseUrl = '@web/quill/1.3.7';
 
-    public $css = ['quill.snow.css'];
-    public $js = ['quill.min.js', 'editor-init.min.js'];
+    public $css = [
+        'quill.snow.css',
+        'highlight/default.min.css',
+    ];
+
+    public $js = [
+        'highlight/highlight.min.js',
+        'quill.min.js',
+        'editor-init.min.js',
+    ];
 
     public $jsOptions = ['defer' => true];
 
