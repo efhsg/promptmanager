2a8d724acaefacd4024fee4afd1c941ef429cb32
erwin <efhsg@live.nl>
2025-08-08 12:59:23 +0200

    Simplify tests in `PromptGenerationServiceTest` by removing redundant scenarios and consolidating dataset definitions.

--- yii/tests/unit/services/PromptGenerationServiceTest.php
+++ yii/tests/unit/services/PromptGenerationServiceTest.php
@@ -136,72 +136,7 @@ class PromptGenerationServiceTest extends Unit
         $ctxD = '{"ops":[{"insert":"Context with "},{"attributes":{"bold":true},"insert":"formatting"},{"insert":" and "},{"attributes":{"italic":true},"insert":"styles"},{"insert":".\n"}]}';
 
         return [
-            'Simple field replacement' => [
-                '{"ops":[{"insert":"Testtemplate GEN:{{1}}\n"}]}',
-                [],
-                [
-                    1 => '{"ops":[{"insert":"Simple text\n"}]}'
-                ],
-                '{"ops":[{"insert":"Testtemplate "},{"insert":"Simple text\n"},{"insert":"\n"}]}'
-            ],
-            'Single field replacement with code' => [
-                '{"ops":[{"insert":"Testtemplate GEN:{{1}}\n"}]}',
-                [$ctxA],
-                [
-                    1 => '{"ops":[{"insert":"<?php"},{"attributes":{"code-block":"plain"},"insert":"\n"},{"insert":"/** @noinspection PhpUnused */"},{"attributes":{"code-block":"plain"},"insert":"\n\n"},{"insert":"namespace common\\constants;"},{"attributes":{"code-block":"plain"},"insert":"\n\n"},{"insert":"class FieldConstants"},{"attributes":{"code-block":"plain"},"insert":"\n"},{"insert":"{"},{"attributes":{"code-block":"plain"},"insert":"\n"},{"insert":"    public const TYPES = [\'text\', \'select\', \'multi-select\', \'code\', \'select-invert\'];"},{"attributes":{"code-block":"plain"},"insert":"\n"},{"insert":"    public const OPTION_FIELD_TYPES = [\'select\', \'multi-select\', \'select-invert\'];"},{"attributes":{"code-block":"plain"},"insert":"\n"},{"insert":"    public const NO_OPTION_FIELD_TYPES = \'input, textarea, select, code\';"},{"attributes":{"code-block":"plain"},"insert":"\n"},{"insert":"}"},{"attributes":{"code-block":"plain"},"insert":"\n"},{"insert":"\n"}]}'
-                ],
-                '{"ops":[{"insert":"Context A line 1\nContext A line 2\n"},{"insert":"Testtemplate\n<?php"},{"attributes":{"code-block":"plain"},"insert":"\n"},{"insert":"/** @noinspection PhpUnused */"},{"attributes":{"code-block":"plain"},"insert":"\n\n"},{"insert":"namespace common\\constants;"},{"attributes":{"code-block":"plain"},"insert":"\n\n"},{"insert":"class FieldConstants"},{"attributes":{"code-block":"plain"},"insert":"\n"},{"insert":"{"},{"attributes":{"code-block":"plain"},"insert":"\n"},{"insert":"    public const TYPES = [\'text\', \'select\', \'multi-select\', \'code\', \'select-invert\'];"},{"attributes":{"code-block":"plain"},"insert":"\n"},{"insert":"    public const OPTION_FIELD_TYPES = [\'select\', \'multi-select\', \'select-invert\'];"},{"attributes":{"code-block":"plain"},"insert":"\n"},{"insert":"    public const NO_OPTION_FIELD_TYPES = \'input, textarea, select, code\';"},{"attributes":{"code-block":"plain"},"insert":"\n"},{"insert":"}"},{"attributes":{"code-block":"plain"},"insert":"\n"},{"insert":"\n"}]}'
-            ],
-            'Multiple text fields' => [
-                '{"ops":[{"insert":"First field: GEN:{{1}}\nSecond field: GEN:{{2}}\nThird field: GEN:{{3}}\n"}]}',
-                [],
-                [
-                    1 => '{"ops":[{"insert":"Text for field 1"}]}',
-                    2 => '{"ops":[{"insert":"Text for field 2"}]}',
-                    3 => '{"ops":[{"insert":"Text for field 3"}]}'
-                ],
-                '{"ops":[{"insert":"First field: Text for field 1\nSecond field: Text for field 2\nThird field: Text for field 3\n"}]}'
-            ],
-            'Mixed field types' => [
-                '{"ops":[{"insert":"Text field: GEN:{{1}}\nCode field: GEN:{{2}}\nSelect field: GEN:{{3}}\n"}]}',
-                [],
-                [
-                    1 => '{"ops":[{"insert":"This is regular text"}]}',
-                    2 => '{"ops":[{"insert":"function example() {"},{"attributes":{"code-block":"javascript"},"insert":"\n"},{"insert":"  return \"Hello World\";"},{"attributes":{"code-block":"javascript"},"insert":"\n"},{"insert":"}"},{"attributes":{"code-block":"javascript"},"insert":"\n"},{"insert":"\n"}]}',
-                    3 => '{"ops":[{"insert":"Option B"},{"attributes":{"bold":true},"insert":" (selected from options A, B, C)"}]}'
-                ],
-                '{"ops":[{"insert":"Text field: This is regular text\nCode field: \nfunction example() {"},{"attributes":{"code-block":"javascript"},"insert":"\n"},{"insert":"  return \"Hello World\";"},{"attributes":{"code-block":"javascript"},"insert":"\n"},{"insert":"}"},{"attributes":{"code-block":"javascript"},"insert":"\n"},{"insert":"\nSelect field: Option B"},{"attributes":{"bold":true},"insert":" (selected from options A, B, C)"},{"insert":"\n"}]}'
-            ],
-            'Nested fields with formatting' => [
-                '{"ops":[{"insert":"Form input:\n"},{"attributes":{"header":2},"insert":"\n"},{"insert":"- Name: GEN:{{1}}\n- Email: GEN:{{2}}\n- Message: GEN:{{3}}\n- Options: GEN:{{4}}\n"}]}',
-                [],
-                [
-                    1 => '{"ops":[{"attributes":{"bold":true},"insert":"John Doe"}]}',
-                    2 => '{"ops":[{"attributes":{"italic":true},"insert":"john.doe@example.com"}]}',
-                    3 => '{"ops":[{"insert":"This is a multiline message.\nIt contains several paragraphs.\n\nAnd some formatting."}]}',
-                    4 => '{"ops":[{"insert":"Selected options:"},{"attributes":{"list":"bullet"},"insert":"\n"},{"insert":"Option 1"},{"attributes":{"list":"bullet"},"insert":"\n"},{"insert":"Option 3"},{"attributes":{"list":"bullet"},"insert":"\n"}]}'
-                ],
-                '{"ops":[{"insert":"Form input:\n"},{"attributes":{"header":2},"insert":"\n"},{"insert":"- Name: "},{"attributes":{"bold":true},"insert":"John Doe"},{"insert":"\n- Email: "},{"attributes":{"italic":true},"insert":"john.doe@example.com"},{"insert":"\n- Message: This is a multiline message.\nIt contains several paragraphs.\n\nAnd some formatting.\n- Options: Selected options:"},{"attributes":{"list":"bullet"},"insert":"\n"},{"insert":"Option 1"},{"attributes":{"list":"bullet"},"insert":"\n"},{"insert":"Option 3"},{"attributes":{"list":"bullet"},"insert":"\n"}]}'
-            ],
-            'Fields with code and multi-select' => [
-                '{"ops":[{"insert":"Database setup:\nConnection string: GEN:{{1}}\nSQL Query: GEN:{{2}}\nSelected tables: GEN:{{3}}\n"}]}',
-                [],
-                [
-                    1 => '{"ops":[{"insert":"mysql://user:password@localhost:3306/database"}]}',
-                    2 => '{"ops":[{"insert":"SELECT"},{"attributes":{"code-block":"sql"},"insert":"\n"},{"insert":"  id, name, email"},{"attributes":{"code-block":"sql"},"insert":"\n"},{"insert":"FROM"},{"attributes":{"code-block":"sql"},"insert":"\n"},{"insert":"  users"},{"attributes":{"code-block":"sql"},"insert":"\n"},{"insert":"WHERE"},{"attributes":{"code-block":"sql"},"insert":"\n"},{"insert":"  active = 1"},{"attributes":{"code-block":"sql"},"insert":"\n"}]}',
-                    3 => '{"ops":[{"insert":"Tables selected: "},{"attributes":{"bold":true},"insert":"users, orders, products"},{"insert":"\nFilters: "},{"attributes":{"italic":true},"insert":"active records only"}]}'
-                ],
-                '{"ops":[{"insert":"Database setup:\nConnection string: mysql://user:password@localhost:3306/database\nSQL Query: \nSELECT"},{"attributes":{"code-block":"sql"},"insert":"\n"},{"insert":"  id, name, email"},{"attributes":{"code-block":"sql"},"insert":"\n"},{"insert":"FROM"},{"attributes":{"code-block":"sql"},"insert":"\n"},{"insert":"  users"},{"attributes":{"code-block":"sql"},"insert":"\n"},{"insert":"WHERE"},{"attributes":{"code-block":"sql"},"insert":"\n"},{"insert":"  active = 1"},{"attributes":{"code-block":"sql"},"insert":"\n"},{"insert":"Selected tables: Tables selected: "},{"attributes":{"bold":true},"insert":"users, orders, products"},{"insert":"\nFilters: "},{"attributes":{"italic":true},"insert":"active records only"},{"insert":"\n"}]}'
-            ],
-            'GEN fields and PRJ fields' => [
-                '{"ops":[{"insert":"Begin testtemplate GEN:{{1}} and PRJ:{{6}} end\n"}]}',
-                [],
-                [
-                    1 => '{"ops":[{"insert":"Het"},{"attributes":{"list":"ordered"},"insert":"\n"},{"insert":"eerste"},{"attributes":{"list":"ordered"},"insert":"\n"},{"insert":"veld"},{"attributes":{"list":"ordered"},"insert":"\n"}]}',
-                    6 => '{"ops":[{"insert":"Het "},{"attributes":{"bold":true},"insert":"tweede"},{"insert":" veld\n"}]}',
-                ],
-                '{"ops": [     {"insert": "Begin testtemplate\n"},     {"insert": "Het"},     {"attributes": {"list": "ordered"}, "insert": "\n"},     {"insert": "eerste"},     {"attributes": {"list": "ordered"}, "insert": "\n"},     {"insert": "veld"},     {"attributes": {"list": "ordered"}, "insert": "\n"},     {"insert": "and\n"},     {"insert": "Het "},     {"attributes": {"bold": true}, "insert": "tweede"},     {"insert": " veld\nend\n"}   ] }'
-            ],
+
             'Change functionality' => [
                 '{"ops":[{"insert":"This is the code we want to change:GEN:{{1}}\nAnd this is the change I want: GEN:{{4}}\nGEN:{{3}}\n"}]}',
                 [],
@@ -210,7 +145,12 @@ class PromptGenerationServiceTest extends Unit
                     6 => '{"ops":[{"insert":"b\n"}]}',
                     3 => [0 => 'do not write any comment', 1 => 'use SOLID, DRY, YAGNI principles'],
                 ],
-                '{"ops":[{"insert":"This is the code we want to change:a\nAnd this is the change I want: b\ndo not write any comment, use SOLID, DRY, YAGNI principles\n"}]}'
+                // updated to reflect two separate inserts
+                '{"ops":[
+                    {"insert":"This is the code we want to change:a\nAnd this is the change I want: b\n"},
+                    {"insert":"do not write any comment.\n"},
+                    {"insert":"use SOLID, DRY, YAGNI principles.\n"}
+                ]}'
             ],
             'Change functionality2' => [
                 '{"ops":[{"insert":"This is the code we want to change:GEN:{{1}}\nAnd this is the change I want: GEN:{{4}}\nGEN:{{3}}\n"}]}',
