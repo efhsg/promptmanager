88cd7f14317f32ceadf7538879ce5a0c366aba40
efhsg <efhsg@live.nl>
2026-02-17 03:52:57 +0100

    FIX: move class_alias after Application constructor for autoloader availability

--- yii/web/index.php
+++ yii/web/index.php
@@ -9,9 +9,11 @@ defined('YII_ENV') or define('YII_ENV', 'dev');
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
+$config = require __DIR__ . '/../config/web.php';
+
+$app = new yii\web\Application($config);
+
 // Backward compat: queued jobs serialized before rename can still deserialize
 class_alias('app\jobs\RunAiJob', 'app\jobs\RunClaudeJob');
 
-$config = require __DIR__ . '/../config/web.php';
-
-(new yii\web\Application($config))->run();
+$app->run();
